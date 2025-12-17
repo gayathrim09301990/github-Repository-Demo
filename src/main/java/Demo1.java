505eb000f482d740a855826397c6e17a207ba1a7
gayathrim09301990 <gayathrim09301990@gmail.com>
2025-12-17 18:28:37 +0530

    init commit1

---
+++ src/main/java/Demo1.java
@@ -0,0 +1,13 @@
+public class Demo1 {
+
+    public static void main(String[] args){
+
+        int i=9;
+        for(int l=1;l<=10;l++) {
+                int t= i*l;
+            System.out.println( i+" * "+l+" = "+t);
+
+        }
+
+    }
+}
