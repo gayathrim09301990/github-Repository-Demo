d188bd1b9b86f887a6e2ba95d7bfb4a08c9da995
gayathrim09301990 <gayathrim09301990@gmail.com>
2025-12-25 16:19:41 +0530

    init commit dec25th

---
+++ src/main/java/org/example/Lab_ternary_operator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Lab_ternary_operator {
+
+    public static void main(String[] args){
+
+        int a=10;
+        String b= a>=0 ? "+ve" : "-ve";
+
+        System.out.println(b);
+
+    }
+}
