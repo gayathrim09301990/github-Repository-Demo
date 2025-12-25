0fddb70111ef4c3b3e1e426ee44b96e8c16adc5d
gayathrim09301990 <gayathrim09301990@gmail.com>
2025-12-25 16:50:57 +0530

    init commit dec25th 2

---
+++ src/main/java/org/example/Lab_CLI_excercise.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Lab_CLI_excercise {
+    public static void main(String[] args){
+        String age_input_string= args[3];
+        System.out.println(age_input_string != null);
+
+        assert age_input_string != null;
+        int age_user_input=Integer.parseInt(age_input_string);
+
+        String result= (age_user_input<=18) ? "minor" : (age_user_input<=60) ? "Adult" : "Sr.citizen" ;
+        System.out.println(result);
+    }
+
+}
