641f6e5db647d2e991ebb417de4bec47fc20d3f0
gayathrim09301990 <gayathrim09301990@gmail.com>
2026-01-06 12:03:21 +0530

    jan 6th 2026 -12.03pm

--- src/main/java/Lab_switch_program.java
+++ src/main/java/Lab_switch_program.java
@@ -6,36 +6,43 @@ public class Lab_switch_program {
 
         System.out.print("enter the number from 1 to 7:" );
         Scanner sc = new Scanner(System.in);
-        int day = sc.nextInt();
+        if(sc.hasNextInt()) {
 
-        switch (day) {
+            int day = sc.nextInt();
+            switch (day) {
 
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("wednesday");
-                break;
-            case 4:
-                System.out.println("thursday");
-                break;
-            case 5:
-                System.out.println("friday");
-                break;
-            case 6:
-                System.out.println("saturday");
-                break;
-            case 7:
-                System.out.println("sunday");
-                break;
+                case 1:
+                    System.out.println("Monday");
+                    break;
+                case 2:
+                    System.out.println("Tuesday");
+                    break;
+                case 3:
+                    System.out.println("wednesday");
+                    break;
+                case 4:
+                    System.out.println("thursday");
+                    break;
+                case 5:
+                    System.out.println("friday");
+                    break;
+                case 6:
+                    System.out.println("saturday");
+                    break;
+                case 7:
+                    System.out.println("sunday");
+                    break;
 
-            default:
-                System.out.println("Please enter the valid day num from 1 to 7!!");
-                break;
+                default:
+                    System.out.println("Please enter the valid day num from 1 to 7!!");
+                    break;
 
+            }
         }
+        else {
+                System.out.println("invalid data entered");
+            }
+
+        System.out.println("End of the program!!");
     }
 }
